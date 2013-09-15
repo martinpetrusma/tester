688204f022955d03feb6c0ef93e879df4e9da5b4
martinpetrusma <martinpok.com>
2013-09-15 21:24:47 +0200

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Tester::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root to: 'index#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
